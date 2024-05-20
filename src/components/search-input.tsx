9bb1226bbb6544e5cb7fea073f55001d2d9c9b9f
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-05-20 22:51:18 +0400

    Add - Removed search input from navbar and moved it to its own component

---
+++ src/components/search-input.tsx
@@ -0,0 +1,20 @@
+import { Search } from "lucide-react";
+import React from "react";
+import { Input } from "@/components/ui/input";
+
+const SearchInput = () => {
+  return (
+    <form className="ml-auto flex-1 sm:flex-initial">
+      <div className="relative">
+        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+        <Input
+          type="search"
+          placeholder="Search products..."
+          className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchInput;

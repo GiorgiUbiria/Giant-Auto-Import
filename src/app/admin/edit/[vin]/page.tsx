21ddad2e0d9b4fcef48ff349b248237a3eba738f
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-06-01 17:39:33 +0400

    Add - Auth check for the admin/edit route

--- src/app/admin/edit/[vin]/page.tsx
+++ src/app/admin/edit/[vin]/page.tsx
@@ -1,8 +1,14 @@
 import EditCarForm from "@/components/edit-car-form";
 import { getCarFromDatabase } from "@/lib/actions/dbActions";
 import { DbCar } from "@/lib/interfaces";
+import { validateRequest } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Page({ params }: { params: { vin: string } }) {
+  const { user } = await validateRequest();
+  if (!user || user?.role_id !== 2) {
+    return redirect("/");
+  }
   const car: DbCar | undefined = await getCarFromDatabase(params.vin);
   return <EditCarForm car={car!} />;
 }

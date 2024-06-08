0d67eea1bd8626030d4e0c01d42ce8d1bf290541
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-06-08 16:12:18 +0400

    Add - Page functions to make terms and privacy pages a module

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div>
+      <div className="mx-auto max-w-3xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
+        <div className="text-center">
+          <h1> Privacy Policy </h1>
+        </div>
+      </div>
+    </div>
+  );
+}

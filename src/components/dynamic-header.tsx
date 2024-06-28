cf4e532fb42a6eae8299ac343e03e3aadca785b9
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-06-28 19:44:03 +0400

    Fix - Updated dynamic header logic

--- src/components/dynamic-header.tsx
+++ src/components/dynamic-header.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import { useEffect, useState, useCallback, useRef } from "react";
-import "../lib/header.css";
+import { useEffect, useRef, useState, useCallback } from "react";
 
 export default function DynamicHeader({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [isScrolledUp, setIsScrolledUp] = useState(false);
   const [isVisible, setIsVisible] = useState(true);
-  const scrollTimer = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const scrollTimer = useRef<NodeJS.Timeout | null>(null);
+
+  const resetVisibilityTimeout = useCallback(() => {
+    setIsVisible(true);
+    if (scrollTimer.current !== null) {
+      clearTimeout(scrollTimer.current);
+    }
+    scrollTimer.current = setTimeout(() => {
+      setIsVisible(false);
+    }, 2000);
+  }, []);
 
   const handleMouseEnter = useCallback(() => {
     setIsVisible(true);
@@ -19,60 +27,35 @@ export default function DynamicHeader({
     }
   }, []);
 
-  const resetScrollTimeout = useCallback(() => {
-    setIsVisible(true);
-    if (scrollTimer.current !== null) {
-      clearTimeout(scrollTimer.current);
-    }
-    scrollTimer.current = setTimeout(() => {
-      setIsVisible(false);
-    }, 3500);
-  }, []);
-
   const handleMouseLeave = useCallback(() => {
-    if (!isScrolledUp) {
-      resetScrollTimeout();
-    }
-  }, [isScrolledUp, resetScrollTimeout]);
+    resetVisibilityTimeout();
+  }, [resetVisibilityTimeout]);
 
   useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY < lastScrollY && screen.width <= 640) {
+        setIsVisible(true);
+        resetVisibilityTimeout();
+      }
+      lastScrollY = currentScrollY;
+    };
+
     if (screen.width <= 640) {
-      let lastScrollY = window.scrollY;
-
-      const handleScroll = () => {
-        const currentScrollY = window.scrollY;
-
-        if (currentScrollY < lastScrollY) {
-          setIsScrolledUp(true);
-          setIsVisible(true);
-          resetScrollTimeout();
-        } else {
-          setIsScrolledUp(false);
-        }
-
-        lastScrollY = currentScrollY;
-      };
-
       window.addEventListener("scroll", handleScroll);
-
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-        if (scrollTimer.current !== null) {
-          clearTimeout(scrollTimer.current);
-        }
-      };
-    } else {
-      setIsVisible(true);
-      setIsScrolledUp(true);
-      return () => {};
+      return () => window.removeEventListener("scroll", handleScroll);
     }
-  }, [resetScrollTimeout]);
+  }, [resetVisibilityTimeout]);
+
+  const className = `fixed bottom-0 left-0 right-0 md:relative md:top-0 flex h-20 items-center gap-4 bg-muted z-10 px-4 md:px-6 shadow-blac-5 ${isVisible ? "" : "header-hidden"}`;
 
   return (
     <header
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className={`fixed bottom-0 left-0 right-0 md:relative md:top-0 flex h-20 items-center gap-4 bg-muted z-10 px-4 md:px-6 shadow-blac-5 ${isScrolledUp && isVisible ? "header-visible" : "header-hidden"}`}
+      className={className}
     >
       {children}
     </header>

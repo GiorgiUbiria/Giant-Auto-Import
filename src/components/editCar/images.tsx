e5eea114ed8a7c7a36307d780eef139d8e1b70fa
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-06-29 21:17:25 +0400

    Add - Alert Dialog component and its usage in Edit car page for deleting a car image

--- src/components/editCar/images.tsx
+++ src/components/editCar/images.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import { Image as ImageType } from "@/lib/interfaces";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 import { toast } from "sonner";
+import { deleteImage } from "@/lib/actions/imageActions";
 
 export default function Images({ images }: { images: ImageType[] }) {
   const [loading, setTransitioning] = React.useTransition();
@@ -27,9 +35,9 @@ export default function Images({ images }: { images: ImageType[] }) {
     imageChunks = chunkArray(images, 3);
   }
 
-  async function deleteImage(imageUrl: string) {
+  async function deleteImageCall(imageUrl: string) {
     setTransitioning(async () => {
-      const res = await ();
+      const res = await deleteImage(imageUrl);
       if (res.error !== null) {
         toast.error(res.error);
         console.error(res.error);
@@ -58,9 +66,34 @@ export default function Images({ images }: { images: ImageType[] }) {
                 </DialogTrigger>
                 <DialogContent className="min-w-full md:min-w-fit">
                   <div className="flex flex-col gap-4">
-                    <Button className="w-full" onClick={() => deleteImage(image.imageUrl!)}>
-                      Delete Image
-                    </Button>
+                    <AlertDialog>
+                      <AlertDialogTrigger asChild>
+                        <Button variant="outline">Delete Image</Button>
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>Delete Image</AlertDialogTitle>
+                          <AlertDialogDescription>
+                            Are you sure you want to delete this image?
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <AlertDialogCancel asChild>
+                            <Button variant="outline">Cancel</Button>
+                          </AlertDialogCancel>
+                          <AlertDialogAction asChild>
+                            <Button
+                              variant="destructive"
+                              onClick={async () =>
+                                await deleteImageCall(image.imageUrl!)
+                              }
+                            >
+                              Delete
+                            </Button>
+                          </AlertDialogAction>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog>
                     <Image
                       className="h-auto min-w-full max-w-full rounded-lg"
                       src={image.imageUrl!}

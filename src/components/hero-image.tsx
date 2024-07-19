6a5f50707309d5f010b586c8d34b7d734b47e97e
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-07-19 18:29:26 +0400

    Add - Optimization of assets. Removed styling on landing page

--- src/components/hero-image.tsx
+++ src/components/hero-image.tsx
@@ -1,42 +1,16 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
-import HeroImage from "../../public/hero-image.avif";
 import HeroBackgroundImage from "../../public/background2.avif";
 
 export default function HeroImageComponent() {
-  const [currentImage, setCurrentImage] = useState(HeroImage);
-  const [animating, setIsAnimating] = useState(false);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsAnimating(true);
-    }, 0)
-    
-    const interval = setInterval(() => {
-      setIsAnimating(true);
-      setCurrentImage((prevImage) =>
-        prevImage === HeroImage ? HeroBackgroundImage : HeroImage,
-      );
-    }, 10000);
-
-    return () => {
-      clearInterval(interval);
-      clearInterval(timeout);
-    };
-  }, []);
-
   return (
     <div className="absolute inset-0 overflow-hidden">
       <Image
-        src={currentImage}
+        src={HeroBackgroundImage}
         alt="Background Image"
         layout="fill"
         objectFit="cover"
         objectPosition="center"
-        className={`${animating ? "animate-zoomIn" : ""}`}
         priority
       />
       <div className="absolute inset-0 bg-black opacity-50"></div>

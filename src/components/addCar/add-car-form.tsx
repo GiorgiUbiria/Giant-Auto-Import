d1c7fdc98cc20ac51aa9bba7ac473c803aba6f9a
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-07-21 22:27:06 +0400

    Add - Daily minor change

--- src/components/addCar/add-car-form.tsx
+++ src/components/addCar/add-car-form.tsx
@@ -10,7 +10,7 @@ export default async function AddCarForm() {
 
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col gap-8">
-      <h1 className="text-4xl font-bold mb-4 text-primary">Add New Car <span>🚗</span>{" "}</h1>
+      <h1 className="text-4xl font-bold mb-4 text-primary">Add a New Car <span>🚗</span>{" "}</h1>
       <AddForm />
     </div>
   );

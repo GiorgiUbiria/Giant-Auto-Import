aab5d97f6f8a911c5c71ccc64beefcb48846d134
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-08-12 17:33:53 +0400

    Add - React Query. Query Provider. ZSA React Query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
 import "./globals.css";
+import ReactQueryProvider from "@/providers/react-query";
 
 export const metadata: Metadata = {
   title: "Giant Auto Import",
@@ -22,27 +23,29 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>
-        <SpeedInsights />
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className="flex min-h-screen w-full flex-col dark:bg-gray-900 bg-gray-300">
-            <Navbar />
-            <main className="flex flex-1 flex-col gap-4 md:gap-8">
-              {children}
-            </main>
-            <Footer />
-            <Toaster 
-              closeButton={true}
-              invert={true}
-              expand={true}
-              richColors={true}
-            />
-          </div>
-        </ThemeProvider>
+        <ReactQueryProvider>
+          <SpeedInsights />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="flex min-h-screen w-full flex-col dark:bg-gray-900 bg-gray-300">
+              <Navbar />
+              <main className="flex flex-1 flex-col gap-4 md:gap-8">
+                {children}
+              </main>
+              <Footer />
+              <Toaster
+                closeButton={true}
+                invert={true}
+                expand={true}
+                richColors={true}
+              />
+            </div>
+          </ThemeProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   );

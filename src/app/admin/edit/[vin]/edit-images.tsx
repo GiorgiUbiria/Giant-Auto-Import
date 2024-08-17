43fe149caef18608bc7345fdc1177fdfd67a69b0
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-08-17 12:45:46 +0400

    Add - Image delete and Make main logic on edit images page

--- src/app/admin/edit/[vin]/edit-images.tsx
+++ src/app/admin/edit/[vin]/edit-images.tsx
@@ -1,18 +1,61 @@
 "use client";
 
-import { getImagesAction } from "@/lib/actions/imageActions";
-import { useServerActionQuery } from "@/lib/hooks/server-action-hooks";
-import { Loader2 } from "lucide-react";
+import { deleteImageAction, getImagesAction, makeImageMainAction } from "@/lib/actions/imageActions";
+import { useServerActionMutation, useServerActionQuery } from "@/lib/hooks/server-action-hooks";
+import { Loader2, Stamp, Trash } from "lucide-react";
 import Image from "next/image";
 import { ImagesForm } from "./images-form";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { useQueryClient } from "@tanstack/react-query";
 
 export const EditImages = ({ vin }: { vin: string }) => {
+	const queryClient = useQueryClient();
+
 	const { isLoading, data } = useServerActionQuery(getImagesAction, {
 		input: {
 			vin: vin,
 		},
 		queryKey: ["getImagesForCar", vin],
 	})
+
+	const { isPending: deletePending, mutate: deleteMutate } = useServerActionMutation(deleteImageAction, {
+		onError: (error) => {
+			const errorMessage = error?.data || "Failed to delete the image";
+			toast.error(errorMessage);
+		},
+		onSuccess: async () => {
+			const successMessage = "Image deleted successfully!";
+			toast.success(successMessage);
+
+			await queryClient.invalidateQueries({
+				queryKey: ["getImagesForCar", vin],
+				refetchType: "active",
+			});
+		},
+	});
+
+	const { isPending: makeMainPending, mutate: makeMainMutate } = useServerActionMutation(makeImageMainAction, {
+		onError: (error) => {
+			const errorMessage = error?.data || "Failed to make the image main";
+			toast.error(errorMessage);
+		},
+		onSuccess: async () => {
+			const successMessage = "Image prioritized successfully!";
+			toast.success(successMessage);
+
+			await queryClient.invalidateQueries({
+				queryKey: ["getImagesForCar", vin],
+				refetchType: "active",
+			});
+		},
+	});
 
 	const LoadingState = () => {
 		return (
@@ -29,14 +72,45 @@ export const EditImages = ({ vin }: { vin: string }) => {
 				{
 					isLoading ? <LoadingState /> : (
 						data?.map((image) => (
-							<Image
-								style={{ transform: "translate3d(0, 0, 0)" }}
+							<TooltipProvider
 								key={image.imageKey}
-								src={image.url}
-								alt="Image"
-								height="300"
-								width="300"
-							/>
+							>
+								<Tooltip>
+									<TooltipTrigger>
+										<Image
+											style={{ transform: "translate3d(0, 0, 0)" }}
+											src={image.url}
+											alt="Image"
+											height="300"
+											width="300"
+										/>
+									</TooltipTrigger>
+									<TooltipContent>
+										<div className="flex gap-x-2">
+											<Button
+												variant="link"
+												size="icon"
+												disabled={makeMainPending || deletePending}
+												onClick={() => {
+													deleteMutate({ imageKey: image.imageKey });
+												}}
+											>
+												<Trash className="size-4 hover:opacity-50 hover:text-red-500 transition" />
+											</Button>
+											<Button
+												variant="link"
+												size="icon"
+												disabled={makeMainPending || deletePending}
+												onClick={() => {
+													makeMainMutate({ imageKey: image.imageKey });
+												}}
+											>
+												<Stamp className="size-4 hover:opacity-50 hover:text-red-500 transition" />
+											</Button>
+										</div>
+									</TooltipContent>
+								</Tooltip>
+							</TooltipProvider>
 						))
 					)
 				}

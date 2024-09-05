fb6fc49d68fa75b1d55a01d0d5303dc4eba4a34e
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-09-05 16:27:04 +0400

    Add - Error handling

--- src/app/admin/cars/client.tsx
+++ src/app/admin/cars/client.tsx
@@ -24,6 +24,7 @@ export const Client = () => {
     return (
       <div className="w-full h-[400px] flex justify-center items-center">
         <p>Error loading car data. Please try again later.</p>
+        <p>{error?.message}</p>
       </div>
     );
   };

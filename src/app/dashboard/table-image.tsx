75cc532e45fa9e3f02bc52d9f4365b9737a3adad
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2025-07-24 15:03:04 +0400

    fix: image and purchase date display on tables

--- src/app/dashboard/table-image.tsx
+++ src/app/dashboard/table-image.tsx
@@ -16,18 +16,18 @@ export const TableImage = ({ vin }: { vin: string }) => {
 
   if (!data && !isLoading) {
     return (
-      <div className="w-[154px] h-[72px] flex justify-center ml-8">
-        <Image
-          alt="Car Image"
-          className="rounded-md object-cover"
-          height={72}
-          width={154}
-          src={NoImage}
-          placeholder="blur"
-          blurDataURL="https://motozitelive.blob.core.windows.net/motozite-live/newcars_images/1670408218No-Image.jpg"
-          loading="lazy"
-        />
-      </div>
+      <div className="absolute inset-0 w-full h-full">
+      <Image
+        alt="Car Image"
+        className="object-cover"
+        fill
+        src={NoImage}
+        placeholder="blur"
+        blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R7/2Q=="
+        loading="lazy"
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
     );
   }
 
@@ -37,15 +37,19 @@ export const TableImage = ({ vin }: { vin: string }) => {
         <Loader2 className="animate-spin" />
       ) : (
         <Image
-          alt="Car Image"
-          className="rounded-md object-cover"
-          height={72}
-          width={154}
-          src={data?.url!}
-          placeholder="blur"
-          blurDataURL="https://motozitelive.blob.core.windows.net/motozite-live/newcars_images/1670408218No-Image.jpg"
-          loading="lazy"
-        />
+        alt="Car Image"
+        className="object-cover"
+        fill
+        src={data?.url!}
+        placeholder="blur"
+        blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECIgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R7/2Q=="
+        loading="lazy"
+        onError={(e) => {
+          console.error('Image failed to load:', data?.url!);
+          e.currentTarget.src = NoImage.src;
+        }}
+        style={{ objectFit: 'cover' }}
+      />
       )}
     </div>
   );
